6f828666d72e51daa876ef8c7a24ff55c64004f3
Cedric Kiplimo <kiplimock@gmail.com>
2020-12-01 18:58:15 +0300

    added no3 solution

--- projecteuler.py
+++ projecteuler.py
@@ -27,6 +27,29 @@ def fib(s):
 			if seq[-1] > 4000000:
 				break
 	return sum([num for num in seq if num%2 == 0])
+	# return seq
 
-print(fib(36))
+print(fib(34))
+
+
+# =========== Number 3 ============ #
+# largest prime factor of 600851475143
+
+d = 3
+n = 600851475143
+primes = []
+
+while d ** 2 < n:
+    if n % d == 0:
+        n /= d
+        primes.append(d)
+    else:
+        d += 2
+        
+# last value of n is the largest prime factor
+primes.append(n)
+# view all primes factors
+print(primes)
+# view largest prime factor 	
+print(max(primes))
 
